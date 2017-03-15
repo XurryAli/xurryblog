e6f76f2d64d5620edd7f49312fc1ba742fff3151
unknown <>
2017-03-15 23:49:50 +0800

    add by xurry 2017-3-15

---
+++ js/common.js
@@ -0,0 +1,28 @@
+
+// Check scroll position and add/remove background to navbar
+function checkScrollPosition() {
+    if($(window).scrollTop() > 50) {
+      $(".fixed-header").addClass("scroll");
+  } else {        
+      $(".fixed-header").removeClass("scroll");
+  }
+}
+
+$(document).ready(function () {   
+    // Single page nav
+    $('.fixed-header').singlePageNav({
+        offset: 59,
+        filter: ':not(.external)',
+        updateHash: true        
+    });
+
+    checkScrollPosition();
+
+     $('.navbar-brand').on('click',function(){
+      window.location.href = "index.html"
+    })
+});
+
+$(window).on("scroll", function() {
+    checkScrollPosition();    
+});
