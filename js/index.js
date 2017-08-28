47b5eb2773617d5c7a4669f2308585dd9f44fd95
徐锐 <xurui@mogoomusic.com>
2017-08-28 17:42:34 +0800

    1.12版本，增加文章列表页显示文章分类

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+/*
+* @Author: Xurry
+* @Date:   2017-08-28 12:04:56
+* @Last Modified by:   Xurry
+* @Last Modified time: 2017-08-28 12:05:01
+*/
+
 $('.travelThis').on('click',function(){
 	window.location.href = "article-list.html?t=" +new Date().getTime();
 })
